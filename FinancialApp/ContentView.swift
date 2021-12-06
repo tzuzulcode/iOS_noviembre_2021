607783777fb98f79af9bf16a039f5466237b8972
Tzuzul Code <mail@tzuzulcode.com>
2021-12-06 09:13:41 -0600

    Initial Commit

---
+++ FinancialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinancialApp
+//
+//  Created by Tzuzul Rosas on 06/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinancialApp/FinancialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAppApp.swift
+//  FinancialApp
+//
+//  Created by Tzuzul Rosas on 06/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
